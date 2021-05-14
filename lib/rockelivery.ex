945a79946dec250e6bdbea80f248886a4254dcb3
Vinicius Sales <vinicius.2010.s@gmail.com>
2021-05-14 12:06:03 -0300

    get user route

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UsersCreate
+  alias Rockelivery.Users.Get, as: UsersGet
 
   defdelegate create_user(params), to: UsersCreate, as: :call
+  defdelegate get_user_by_id(id), to: UsersGet, as: :by_id
 end
